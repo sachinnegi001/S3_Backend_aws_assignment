f359863b139072eedf5872c5f99405ba4017feae
SACHIN SINGH NEGI <55476118+sachinnegi001@users.noreply.github.com>
2023-02-21 00:17:25 +0530

    adding new code

--- main.tf
+++ main.tf
@@ -1,4 +1,32 @@
-resource"aws_s3_bucket" "sachinBucket"{
-    bucket="sachin1 
-    tags =var.Tags
+# resource"aws_s3_bucket" "sachinBucket"{ 
+#     count = length(var.bucket-tags)
+#     bucket=var.bucket-tags[count.index]
+#     tags = {
+#         Name=var.bucket-tags[count.index]
+#         Owner="sachin.negi@cloudeq.com"
+#         Purpose="Training"
+#     }
+    
+#     }
+
+resource "aws_instance" "sachin-ec2" {
+
+    instance_type = "t2.micro"
+    ami = "ami-0e742cca61fb65051"
+
+    tags = {
+      "Name" = "sachin"
+      "Owner" = "sachin.negi@cloudeq.com"
+      "Purpose" = "Training"
+    }
+    volume_tags = {
+      "Name" = "sachin"
+      "Owner" = "sachin.negi@cloudeq.com"
+      "Purpose" = "Training"
+    }
+
+  
+
 }
+
+
