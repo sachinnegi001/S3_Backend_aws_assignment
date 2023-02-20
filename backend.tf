5614787e39e9e929a653180884654be58627505a
SACHIN SINGH NEGI <55476118+sachinnegi001@users.noreply.github.com>
2023-02-21 00:18:51 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = aws_s3_bucket.sachinBucket.id
+    bucket = "sachin-bkt1"
     key    = "sachintffile.tfstate"
     region = "ap-south-1"
     
-    dynamodb_table = "sachin-dynamo"
   }
 }
